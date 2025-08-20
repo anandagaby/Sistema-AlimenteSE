6fb255a815a40387ef0cd40c23f68c080c9deca6
Ananda Gabriely <ananda_alves@estudante.sesisenai.org.br>
2025-08-20 15:24:34 -0300

    Update conexao.php

--- cliente/conexao.php
+++ cliente/conexao.php
@@ -3,7 +3,7 @@
 $usuario = 'root';
 $senha = '';
 $database = 'cantina';
-$host = 'localhost';
+$host = 'localhost:3406';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
 
@@ -11,3 +11,4 @@ if ($mysqli->connect_error) {
     die("Falha ao conectar ao banco de dados! " . $mysqli->connect_error);
 }
 ?>
+
